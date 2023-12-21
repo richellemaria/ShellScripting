fe5e1cc7534dbb85ccb799e0149a42da9d838686
richellemaria <rishdsouza@gmail.com>
2023-12-21 22:37:41 +0530

    auto push

--- 1.colour.sh
+++ 1.colour.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo -e "\e[32m I am prininting green colour \e[0m"
-echo -e "\e[43;32m I am prininting green colour with background colour \e[0m"
+echo -e "\e[43;32m I am prininting green colour with yellow background colour \e[0m"
