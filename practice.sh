2ca2629ddbf171a2cdd4d2aff8085a9597b8e346
richellemaria <rishdsouza@gmail.com>
2023-12-22 14:08:02 +0530

    auto push

---
+++ practice.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e "\e[34m this is foreground colour \e[0m"
+echo -e "\e[34;32m this background with foreground colour \e[0m"
+
+a=10
+b=15
+
+echo "the value of a is $a"
+echo "the value of d is %d"
+
+todays_date=$(date +%d)
+echo -e " todays date is \e[34m $todays_date \e[0m"
+
+echo "the name of the script is $0"
+echo "my name is $1"
+echo "I am learning $2"
+echo $*
+echo $@
+echo $$
+echo $#  
+echo $?
