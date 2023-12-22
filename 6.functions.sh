a0236015302f4c51abf3fec4069347e42404f8f2
richellemaria <rishdsouza@gmail.com>
2023-12-22 14:47:13 +0530

    auto push

--- 6.functions.sh
+++ 6.functions.sh
@@ -12,9 +12,8 @@ sample
 
 status(){
 
-TODAYS_DATE=$(date +%F)
-NO_OF_SESSION=$(who | wc -l)
-echo -e "Todays date is \e[34m  $TODAYS_DATE \e[0m"
 
-echo -e "No of session open \e[34m $NO_OF_SESSION \e[0m" 
+echo -e "Todays date is \e[34m  $(date +%F) \e[0m"
+
+echo -e "No of session open \e[34m $(who | wc -l) \e[0m" 
 }
