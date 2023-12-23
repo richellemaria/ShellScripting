3b1b669d716589bb91db0b2fa2e255a31b3fa767
richellemaria <rishdsouza@gmail.com>
2023-12-23 22:43:25 +0530

    auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,11 +1,49 @@
 #!/bin/bash
 
+COMPONENT=frontend
+LOGFILE=/tmp/$COMPONENT.log
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then
-  echo -e "\e[32m The script is excepted to  be run as sudo user \e[0m"
-
+  echo -e "\e[31m The script is excepted to  be run as sudo user \e[0m"
+  exit 1
 fi
 
-echo "Installing Nginx"
-yum install nginx -y &>> "/tmp/""
+stat(){
+    if [ $1 -eq 0 ] ; then
+      echo -e "\e[32m success \e[0m"
+    else 
+      echo -e "\e[32m failure \e[0m" 
+      exit 2
+    fi    
+}
+
+echo -n "Installing Nginx"
+yum install nginx -y &>> $LOGFILE
+
+stat $?
+
+echo -n "Downloading $COMPONENT component"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+stat $?
+
+echo -n "Performing cleanup"
+cd /usr/share/nginx/html
+rm -rf * &>> $LOGFILE
+
+stat $?
+
+echo -n "extracting $COMPONENT conponent"
+unzip /tmp/$COMPONENT.zip &>> $LOGFILE
+mv $COMPONENT-main/* .
+mv static/* .
+rm -rf $COMPONENT-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
+
+echo -n "starting $COMPONENT conponent"
+systemctl enable nginx &>> $LOGFILE
+systemctl start nginx &>> $LOGFILE
+stat $?
+
 
--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-bash components/$1.sh
+bash components/
