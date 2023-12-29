49d9b6184aaeecec7035a5ed8be43b388cd48872
richellemaria <rishdsouza@gmail.com>
2023-12-29 22:37:03 +0530

    auto push

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -39,8 +39,7 @@ npm install &>> $LOGFILE
 stat $?
 
 echo -n "updating $COMPONENT systemfile"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
-sed -i -e 's/REDIS_DNSNAME/redis.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+sed -i -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
 mv /home/roboshop/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
 stat $?
 
