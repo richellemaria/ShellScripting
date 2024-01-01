4fbfd19ed8ae0046f78b628b745507bd1880fac3
richellemaria <rishdsouza@gmail.com>
2024-01-01 16:34:30 +0530

    auto push

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -5,8 +5,8 @@ COMPONENT=rabbitmq
 source components/common.sh
 
 echo -n "configuring in $COMPONENT"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash 
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>> $LOGFILE
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $LOGFILE
 stat $?
 
 echo -n "installing $COMPONENT"
@@ -19,9 +19,12 @@ systemctl enable rabbitmq-server  &>> $LOGFILE
 systemctl start rabbitmq-server &>> $LOGFILE
 stat $?
 
-echo -n "creating the $COMPONENT $APPUSER"
-rabbitmqctl add_user roboshop roboshop123   
-stat $?
+rabbitmq list_users | grep roboshop &>> $LOGFILE
+if [ $? -ne 0 ] ; then
+    echo -n "creating the $COMPONENT $APPUSER"
+    rabbitmqctl add_user roboshop roboshop123   
+    stat $?
+fi
 
 echo -n "configuring the $COMPONENT $APPUSER privilege"
 rabbitmqctl set_user_tags roboshop administrator
