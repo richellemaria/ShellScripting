e6e26b92ab4df7a85adb2f5e6863d8a02849463e
richellemaria <rishdsouza@gmail.com>
2024-01-01 14:52:20 +0530

    auto push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -12,5 +12,37 @@ stat $?
 echo -n "starting $COMPONENT"
 systemctl enable mysqld $>> $LOGFILE 
 systemctl start mysqld $>> $LOGFILE 
+stat $?
+
+echo -n "changing default password"
+DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | aws -F ' ' '{print $NF}')
+stat $?
+
+echo "show database;" | mysql -uroot $pRoboShop@1 $>> $LOGFILE
+if [ $? -ne 0 ] ; then
+    echo -n "Performing root password reset"
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED by 'RoboShop@1';" | mysql --connect-expired-password -uroot -p$(DEFAULT_ROOT_PASSWORD) $>> $LOGFILE  
+    stat $?
+fi
+
+echo "show pulgins;" | mysql -uroot $pRoboShop@1 | grep validate_password $>> $LOGFILE
+if [ $? -eq 0 ] ; then
+    echo -n "uninstalling password plugin validation"
+    echo "UNINSTALL PLUGIN validate_password;" | mysql -uroot $pRoboShop@1 |  $>> $LOGFILE  
+    stat $?
+fi
+
+echo -n "downloading in $COMPONENT schema"
+curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"  $>> $LOGFILE
+stat $?
+
+echo -n "Extracting the $COMPONENT schema"
+cd /tmp
+unzip -o $COMPONENT.zip
+cd $COMPONENT-main
+mysql -u root -pRoboShop@1 <shipping.sql
+stat $?
+
+
 
 
