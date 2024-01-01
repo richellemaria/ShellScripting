70211561f4e922003e894d2549045eec5fc3de9b
richellemaria <rishdsouza@gmail.com>
2024-01-01 12:19:37 +0530

    auto push

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -17,3 +17,73 @@ stat(){
       exit 2
     fi    
 }
+
+CREATE_USER(){
+
+    id $APPUSER &>> $LOGFILE
+    if [ $? -ne 0 ] ; then
+
+        echo -n "Creating the service account"
+        useradd $APPUSER &>> $LOGFILE
+        stat $?
+
+    fi
+}
+
+DOWNLOAD_EXTRACT(){
+    echo -n "downloading $COMPONENT"
+    curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+    stat $?
+
+    echo -n "copying the $COMPONENT to $APPUSER home directory"
+    cd /home/$APPUSER
+    rm -rf $COMPONENT &>> $LOGFILE
+    unzip -o /tmp/catalogue.zip &>> $LOGFILE
+    stat $?
+
+    echo -n "modifying the owbership"
+    mv $COMPONENT-main/ $COMPONENT
+    chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
+    stat $?
+
+}
+NPM_INSTALL(){
+
+    echo -n "Generating npm $COMPONENT artifacts"
+    cd /home/$APPUSER/$COMPONENT
+    npm install &>> $LOGFILE
+    stat $?
+    
+}
+
+CONFIG_SERVICE(){
+
+    echo -n "updating $COMPONENT systemfile"
+    sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+    mv /home/roboshop/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+    stat $?
+
+    echo -n "starting $COMPONENT service"
+    systemctl daemon-reload &>> $LOGFILE
+    systemctl enable $COMPONENT &>> $LOGFILE
+    systemctl restart $COMPONENT &>> $LOGFILE
+    stat $?
+
+}
+
+NODEJS(){
+
+    echo -n "Configuring and installing the $COMPONENT repo"
+    yum install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y &>> $LOGFILE
+    yum install nodejs -y  &>> $LOGFILE
+    stat $?
+ 
+    CREATE_USER
+
+    DOWNLOAD_EXTRACT
+
+    NPM_INSTALL
+
+    CONFIG_SERVICE
+
+}
--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -4,51 +4,7 @@ COMPONENT=user
 
 source components/common.sh
 
-echo -n "Configuring and installing the $COMPONENT repo"
-yum install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y &>> $LOGFILE
-yum install nodejs -y  &>> $LOGFILE
-stat $?
-
-id $APPUSER &>> $LOGFILE
-if [ $? -ne 0 ] ; then
-
-    echo -n "Creating the service account"
-    useradd $APPUSER &>> $LOGFILE
-    stat $?
-
-fi
-
-echo -n "downloading $COMPONENT"
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-stat $?
-
-echo -n "copying the $COMPONENT to $APPUSER home directory"
-cd /home/$APPUSER
-rm -rf $COMPONENT &>> $LOGFILE
-unzip -o /tmp/$COMPONENT.zip &>> $LOGFILE
-stat $?
-
-echo -n "modifying the owbership"
-mv $COMPONENT-main/ $COMPONENT
-chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
-stat $?
-
-echo -n "Generating npm $COMPONENT artifacts"
-cd /home/$APPUSER/$COMPONENT
-npm install &>> $LOGFILE
-stat $?
-
-echo -n "updating $COMPONENT systemfile"
-sed -i -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
-mv /home/roboshop/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
-stat $?
-
-echo -n "starting $COMPONENT service"
-systemctl daemon-reload &>> $LOGFILE
-systemctl enable $COMPONENT &>> $LOGFILE
-systemctl restart $COMPONENT &>> $LOGFILE
-stat $?
-
+NODEJS
 
 
 
