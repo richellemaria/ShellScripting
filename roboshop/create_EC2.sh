2f8fd295e1e30fca8351fa0e40237d98604b4b2d
richellemaria <rishdsouza@gmail.com>
2024-01-02 22:49:45 +0530

    auto push

--- roboshop/create_EC2.sh
+++ roboshop/create_EC2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 COMPOMENT=$1
+HOSTED_ZONE=Z04725971RVPYECKIUM2P
 
 if [ -z "$1" ] ; then
    echo -e "\e[31m Component name is needed  \e[0m"
@@ -16,3 +17,9 @@ echo -e "security group ID is \e[32m $SG_ID \e[0m"
 echo -n "launching the server"
 IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPOMENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 echo -e "private IP Address \e[32m $IPADDRESS \e[0m"
+
+echo -n "creating DNS record for $COMPOMENT"
+sed -e "s/Component/$COMPOMENT/" -e "s/IPAddress/$IPADDRESS/" roboshop/route53.json > /tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE --change-batch file://tmp/record.json
+
+
