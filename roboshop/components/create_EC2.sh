beef2aa1427cf828f65c1e20c4bf4fc70093405e
richellemaria <rishdsouza@gmail.com>
2024-01-02 20:42:06 +0530

    auto push

--- roboshop/components/create_EC2.sh
+++ roboshop/components/create_EC2.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
+SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=B54-allow-all" |jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 echo -e "AMI is  \e[32m $AMI_ID \e[0m"
+echo -e "security group ID is \e[32m $SG_ID \e[0m"
 
-# echo -n "launching the server"
-# aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro
+echo -n "launching the server"
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro
+
