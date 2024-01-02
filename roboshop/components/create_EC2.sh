b91174dc2cb1358fd808522dd830ee481eb11220
richellemaria <rishdsouza@gmail.com>
2024-01-02 22:27:14 +0530

    auto push

--- roboshop/components/create_EC2.sh
+++ roboshop/components/create_EC2.sh
@@ -9,5 +9,5 @@ echo -e "AMI is  \e[32m $AMI_ID \e[0m"
 echo -e "security group ID is \e[32m $SG_ID \e[0m"
 
 echo -n "launching the server"
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPOMENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g'
-
+IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPOMENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+echo -e "private IP Address \e[32m $IPADDRESS \e[0m"
