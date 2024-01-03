0a55311c4ff77b743c42d9f2ff965094f6549a1c
richellemaria <rishdsouza@gmail.com>
2024-01-03 22:12:28 +0530

    auto push

--- roboshop/create_EC2.sh
+++ roboshop/create_EC2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 COMPOMENT="$1"
+ENV=$2
 HOSTED_ZONE=Z04725971RVPYECKIUM2P
 
-if [ -z "$1" ] ; then
-   echo -e "\e[31m Component name is needed  \e[0m"
+if [ -z "$1" ] || [ -z "$2" ]; then
+   echo -e "\e[31m Component name is needed  and env name is needed \e[0m"
    exit 1
 fi
 
@@ -17,12 +18,12 @@ echo -e "security group ID is \e[32m $SG_ID \e[0m"
 create_EC2(){
 
    echo -n "launching the server"
-   IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPOMENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+   IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPOMENT-$ENV}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
    echo -e "private IP Address \e[32m $IPADDRESS \e[0m"
 
    echo -n "creating DNS record for $COMPOMENT"
-   sed -e "s/Component/$COMPOMENT/" -e "s/IPAddress/$IPADDRESS/" route53.json > /tmp/record.json
-   aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE --change-batch file://tmp/record.json
+   sed -e "s/Component/$COMPOMENT-$ENV/" -e "s/IPAddress/$IPADDRESS/" route53.json > /tmp/record.json
+   aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE --change-batch file:///tmp/record.json
 
 }
 
