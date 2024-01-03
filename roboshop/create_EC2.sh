ab2d39ad598448072f901c0b00f6a01577e93cdb
richellemaria <rishdsouza@gmail.com>
2024-01-03 22:37:45 +0530

    auto push

--- roboshop/create_EC2.sh
+++ roboshop/create_EC2.sh
@@ -18,7 +18,7 @@ echo -e "security group ID is \e[32m $SG_ID \e[0m"
 create_EC2(){
 
    echo -n "launching the $COMPONENT-$ENV server"
-   IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPONENT-$ENV}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+   IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT-$ENV}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
    echo -e "private IP Address \e[32m $IPADDRESS \e[0m"
 
    echo -n "creating DNS record for $COMPONENT"
